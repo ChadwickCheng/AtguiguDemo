6c2ee8814afcf2af15333ca8e9bf15177d117dcd
Dan Cheng <ChadwickCheng@outlook.com>
2025-09-10 08:20:04 +0800

    feat: 完善添加修改0910

--- src/views/product/trademark/index.vue
+++ src/views/product/trademark/index.vue
@@ -21,7 +21,12 @@
         </el-table-column>
         <el-table-column label="品牌操作">
           <template #default="{ row, $index }">
-            <el-button type="primary" size="small" icon="Edit" @click="updateTrademark(row)"></el-button>
+            <el-button
+              type="primary"
+              size="small"
+              icon="Edit"
+              @click="updateTrademark(row)"
+            ></el-button>
             <el-button type="primary" size="small" icon="Delete"></el-button>
           </template>
         </el-table-column>
@@ -41,11 +46,11 @@
     />
     <!-- 对话框 添加和修改使用 -->
     <el-dialog v-model="dialogFormVisible" :title="trademarkParams.id ? '修改品牌' : '添加品牌'">
-      <el-form style="width: 80%">
-        <el-form-item label="品牌名称" label-width="80px">
+      <el-form style="width: 80%" :model="trademarkParams" :rules="rules" ref="formRef">
+        <el-form-item label="品牌名称" label-width="100px" prop="tmName">
           <el-input placeholder="请输入品牌名称" v-model="trademarkParams.tmName"></el-input>
         </el-form-item>
-        <el-form-item label="品牌LOGO" label-width="80px">
+        <el-form-item label="品牌LOGO" label-width="100px" prop="logoUrl">
           <el-upload
             class="avatar-uploader"
             action="/api/admin/product/fileUpload"
@@ -59,16 +64,16 @@
         </el-form-item>
       </el-form>
       <!-- footer插槽 -->
-       <template #footer>
+      <template #footer>
         <el-button type="primary" @click="cancel">取消</el-button>
         <el-button type="primary" @click="confirm">确定</el-button>
-       </template>
+      </template>
     </el-dialog>
-   </div>
+  </div>
 </template>
 
 <script setup lang="ts" name="Trademark">
-import { ref, reactive, onMounted } from 'vue'
+import { ref, reactive, onMounted, nextTick } from 'vue'
 import { reqHasTrademark, reqAddOrUpdateTrademark } from '@/api/product/trademark'
 import type { Records, TradeMarkResponseData, TradeMark } from '@/api/product/trademark/type'
 import type { UploadProps } from 'element-plus'
@@ -88,8 +93,10 @@ const dialogFormVisible = ref<boolean>(false)
 const trademarkParams = reactive<TradeMark>({
   tmName: '',
   logoUrl: '',
-  id: undefined
+  id: undefined,
 })
+// 获取form实例
+const formRef = ref(null)
 
 // 获取商标列表
 const getHasTrademark = async () => {
@@ -125,13 +132,20 @@ const addTradeMark = () => {
   trademarkParams.id = undefined
   trademarkParams.tmName = ''
   trademarkParams.logoUrl = ''
+  // 每次添加前清除表单校验结果 但是第一次点击还没form 使用可选链或nextTick
+  // formRef.value?.clearValidate(['tmName', 'logoUrl'])
+  nextTick(() => {
+    formRef.value?.clearValidate(['tmName', 'logoUrl'])
+  })
 }
 
 // 修改品牌
 const updateTrademark = (row: TradeMark) => {
+  nextTick(() => {
+    formRef.value?.clearValidate(['tmName', 'logoUrl'])
+  })
   dialogFormVisible.value = true
   Object.assign(trademarkParams, row)
-
 }
 
 // 取消按钮
@@ -142,13 +156,16 @@ const cancel = () => {
 // 确定按钮
 // 逻辑正确 由于mock接口并非实际后端所以数据都在内存无法通过getHasTrademark()展示
 const confirm = async () => {
-  dialogFormVisible.value = false
+  // 发请求前触发表单验证
+  await formRef.value.validate()
   const res: any = await reqAddOrUpdateTrademark(trademarkParams)
   if (res.code === 200) {
+    dialogFormVisible.value = false
     ElMessage.success(trademarkParams.id ? '修改成功' : '添加成功')
     getHasTrademark()
   } else {
     ElMessage.error(trademarkParams.id ? '修改失败' : '添加失败')
+    dialogFormVisible.value = false
   }
 }
 
@@ -169,6 +186,43 @@ const beforeAvatarUpload: UploadProps['beforeUpload'] = (file) => {
 // 上传图片成功
 const handleAvatarSuccess: UploadProps['onSuccess'] = (response) => {
   trademarkParams.logoUrl = response.data.url
+  // 图片上传成功清除图片校验结果
+  formRef.value.clearValidate('logoUrl')
+}
+
+// 自定义校验
+const validatorTmName = (rule: any, value: string, callback: any) => {
+  if (value.trim().length >= 2) {
+    callback()
+  } else {
+    callback(new Error('品牌名称至少2个字符'))
+  }
+}
+
+const validatorLogoUrl = (rule: any, value: string, callback: any) => {
+  // 图片上传 value是地址 通过
+  if (value) {
+    callback()
+  } else {
+    callback(new Error('请上传品牌LOGO图片'))
+  }
+}
+
+// 表单校验
+const rules = {
+  tmName: [
+    {
+      required: true, // 字段必须校验，表单项前出现红色五角星
+      trigger: 'blur', // 失去焦点时触发
+      validator: validatorTmName, // 根据trigger触发
+    },
+  ],
+  logoUrl: [
+    {
+      required: true,
+      validator: validatorLogoUrl,
+    },
+  ],
 }
 
 onMounted(() => {
